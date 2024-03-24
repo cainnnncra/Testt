73bfa398e5799ab80ce65df73d6912ea50769713
Cainc <Cainc@192.168.0.110>
2024-03-24 22:55:19 +0530

    second

--- TestProject/src/test/testtttt.java
+++ TestProject/src/test/testtttt.java
@@ -1,5 +1,20 @@
 package test;
 
+import java.util.Scanner;
+
 public class testtttt {
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Cain Crasto \nSYBCA-A\nR/BCA-22-117");
+		 int x, y, sum;
+		 Scanner Obj = new Scanner(System.in); // Create a Scanner object
+		 System.out.println("Enter the number:");
+		 x = Obj.nextInt(); // Read user input
+		 System.out.println("Enter the number:");
+		 y = Obj.nextInt(); // Read user input
+		 sum = x + y; // Calculate the sum of x + y
+		 System.out.println("Sum is: " + sum); // Print the sum
+
+	}
 
 }
